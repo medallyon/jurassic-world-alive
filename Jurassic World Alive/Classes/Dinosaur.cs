4425cfca2192a64db9d1c01cc04693533f773162
Tilman Raendchen <TReandchen@outlook.com>
2018-11-12 01:11:44 +0000

    Create basic Dinosaur class

---
+++ Jurassic World Alive/Classes/Dinosaur.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurassic_World_Alive
+{
+    enum DinosaurType { Carnivorous, Herbivorous }
+    enum DinosaurPeriod { Jurassic, Triassic, Cretaceous }
+
+    class Dinosaur : CircularLinkedListNode
+    {
+        public string Species { get; set; }
+        public DinosaurType Type { get; set; }
+        public DinosaurPeriod Period { get; set; }
+
+        public Dinosaur(CircularLinkedList parentList, string speciesName, DinosaurType type, DinosaurPeriod period) : base(parentList)
+        {
+            this.Species = speciesName;
+            this.Type = type;
+            this.Period = period;
+        }
+    }
+}
